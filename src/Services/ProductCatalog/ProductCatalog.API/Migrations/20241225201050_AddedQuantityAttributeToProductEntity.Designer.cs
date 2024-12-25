e75bab9a07df36618bc8d51dc41ee98ee3a20d9a
nevein25 <neveinabdelfattah@gmail.com>
2024-12-25 22:40:29 +0200

    add missing Quantity property to Product model in ProductCatalog service

--- src/Services/ProductCatalog/ProductCatalog.API/Migrations/20241225201050_AddedQuantityAttributeToProductEntity.Designer.cs
+++ src/Services/ProductCatalog/ProductCatalog.API/Migrations/20241225201050_AddedQuantityAttributeToProductEntity.Designer.cs
@@ -2,6 +2,7 @@
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using ProductCatalog.API.Persistance;
 
@@ -10,9 +11,11 @@ using ProductCatalog.API.Persistance;
 namespace ProductCatalog.API.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20241225201050_AddedQuantityAttributeToProductEntity")]
+    partial class AddedQuantityAttributeToProductEntity
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
--- src/Services/ProductCatalog/ProductCatalog.API/Models/Product.cs
+++ src/Services/ProductCatalog/ProductCatalog.API/Models/Product.cs
@@ -7,6 +7,7 @@ public class Product
     public string Description { get; set; } = default!;
     public string ImageFile { get; set; } = default!;
     public decimal Price { get; set; }
+    public int Quantity { get; set; }
     public Category Category { get; set; } = new();
     public int SellerId { get; set; }
 
