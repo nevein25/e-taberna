b2c51a4790dbf71216a4a53492440a7c4c8e5138
nevein25 <neveinabdelfattah@gmail.com>
2024-12-28 16:41:29 +0200

    add Carter and MediatR packages and register their services

--- src/Services/ShoppingCart/ShoppingCart.API/Extensions/ServiceCollectionExtensions.cs
+++ src/Services/ShoppingCart/ShoppingCart.API/Extensions/ServiceCollectionExtensions.cs
@@ -1,14 +1,23 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using Carter;
+using Microsoft.EntityFrameworkCore;
+using ShoppingCart.API.ApplicationSettings;
 using ShoppingCart.API.Presestance;
+using ShoppingCart.API.Seeders;
+using System.Reflection;
 
 namespace ShoppingCart.API.Extensions;
 
 public static class ServiceCollectionExtensions
 {
-    public static void RegisterServices(this IServiceCollection services, IConfiguration configuration)
+    public static void RegisterServices(this IServiceCollection services, IConfiguration configuration, Assembly assembly)
     {
-        services.AddDbContext<AppDbContext>(options =>
-                options.UseSqlServer(configuration.GetConnectionString("Micro.CartDb")));
+        services.AddDbContext<AppDbContext>(options => options.UseSqlServer(configuration.GetConnectionString("CartDb")));
+
+
+        services.AddMediatR(opt => opt.RegisterServicesFromAssembly(assembly));
+        services.AddCarter();
+
+
 
     }
 }
--- src/Services/ShoppingCart/ShoppingCart.API/Program.cs
+++ src/Services/ShoppingCart/ShoppingCart.API/Program.cs
@@ -11,6 +11,9 @@ var app = builder.Build();
 
 
 
+app.MapCarter();//  maps the routes defined in the ICarterModule implementation.
+                //  scans code and looking for the ICarterModule implementation and map the required Http methods.
+
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
