7624929aa9638cf79e9c690d0037b204faf55a7e
nevein25 <neveinabdelfattah@gmail.com>
2025-01-10 20:28:57 +0200

    Order: add swagger, set up database

---
+++ src/Services/Order/Order.Infrastructure/Extensions/ServiceCollectionExtensions.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Order.Infrastructure.Persistence;
+
+namespace Order.Infrastructure.Extensions;
+public static class ServiceCollectionExtensions
+{
+    public static void AddInfrastructure(this IServiceCollection services, IConfiguration config)
+    {
+        services.AddDbContext<AppDbContext>(opt => opt.UseSqlServer(config.GetConnectionString("OrderDb")));
+    }
+}
